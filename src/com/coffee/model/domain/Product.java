6457766c718b80403f649b795d78615d4b71875f
arammyy <leearam94@gmail.com>
2019-04-25 15:00:42 +0900

    ??

--- src/com/coffee/model/domain/Product.java
+++ src/com/coffee/model/domain/Product.java
@@ -4,55 +4,77 @@ import org.springframework.web.multipart.MultipartFile;
 
 public class Product {
 
-   private int product_id;
-   private Category category;
-   private String name;
-   private String filename;
-   private MultipartFile myFile; // 여기로 파일이 들어옴
-   private int price;
-   private int cost;
-   public int getProduct_id() {
-      return product_id;
-   }
-   public void setProduct_id(int product_id) {
-      this.product_id = product_id;
-   }
-   public Category getCategory() {
-      return category;
-   }
-   public void setCategory(Category category) {
-      this.category = category;
-   }
-   public String getName() {
-      return name;
-   }
-   public void setName(String name) {
-      this.name = name;
-   }
-   public String getFilename() {
-      return filename;
-   }
-   public void setFilename(String filename) {
-      this.filename = filename;
-   }
-   public MultipartFile getMyFile() {
-      return myFile;
-   }
-   public void setMyFile(MultipartFile myFile) {
-      this.myFile = myFile;
-   }
-   public int getPrice() {
-      return price;
-   }
-   public void setPrice(int price) {
-      this.price = price;
-   }
-   public int getCost() {
-      return cost;
-   }
-   public void setCost(int cost) {
-      this.cost = cost;
-   }
-   
-}
+	private int product_id;
+	private Category category;
+	private String name;
+	private String filename;
+	private MultipartFile myFile; // 여기로 파일이 들어옴
+	private int price;
+	private int cost;
+	private String detail;
 
+	public String getDetail() {
+		return detail;
+	}
+
+	public void setDetail(String detail) {
+		this.detail = detail;
+	}
+
+	public int getProduct_id() {
+		return product_id;
+	}
+
+	public void setProduct_id(int product_id) {
+		this.product_id = product_id;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getFilename() {
+		return filename;
+	}
+
+	public void setFilename(String filename) {
+		this.filename = filename;
+	}
+
+	public MultipartFile getMyFile() {
+		return myFile;
+	}
+
+	public void setMyFile(MultipartFile myFile) {
+		this.myFile = myFile;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public int getCost() {
+		return cost;
+	}
+
+	public void setCost(int cost) {
+		this.cost = cost;
+	}
+
+}
